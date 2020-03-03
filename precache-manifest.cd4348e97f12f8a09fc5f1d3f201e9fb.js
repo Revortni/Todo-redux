ba61bfb1e6aef4b0d3bddcbc57f3081b1843c074
revortni <rojit.blaze13@gmail.com>
2020-03-03 20:37:03 +0545

    Updates

---
+++ precache-manifest.cd4348e97f12f8a09fc5f1d3f201e9fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436269f16a7e15cf8aeb6f4b801d3f72",
+    "url": "/Todo-redux/index.html"
+  },
+  {
+    "revision": "f47b80bc4ba6b9fd883f",
+    "url": "/Todo-redux/static/css/main.01f2ddd0.chunk.css"
+  },
+  {
+    "revision": "74ba0bb6cdf808dcb847",
+    "url": "/Todo-redux/static/js/2.580ad43e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Todo-redux/static/js/2.580ad43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47b80bc4ba6b9fd883f",
+    "url": "/Todo-redux/static/js/main.f7c38599.chunk.js"
+  },
+  {
+    "revision": "58109bfd3dea19cdee2f",
+    "url": "/Todo-redux/static/js/runtime-main.ecbc510b.js"
+  }
+]);
